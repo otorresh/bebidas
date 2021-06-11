a5070b8a26f4dc6cf8e81165fccc646da648a681
otorresh <56235680+otorresh@users.noreply.github.com>
2021-06-10 23:51:32 -0500

    Project finished

---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Crear el context
+export const CategoriasContext = createContext();
+
+// Provider es donde se encuentran las funciones y state
+const CategoriasProvider = (props) => {
+  const [categorias, guardarCategorias] = useState([]);
+
+  // Ejecutar el llamado a la API
+  useEffect(() => {
+    const obtenerCategorias = async () => {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+
+      const respuesta = await axios.get(url);
+      guardarCategorias(respuesta.data.drinks);
+    };
+    obtenerCategorias();
+  }, []);
+
+  return (
+    <CategoriasContext.Provider
+      value={{
+        categorias
+      }}
+    >
+      { props.children }
+    </CategoriasContext.Provider>
+  );
+};
+
+export default CategoriasProvider;
